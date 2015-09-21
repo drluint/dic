5ea67e48f8a3e42fbd680f45fe757f4e5056bfc3
errorld <errorld@outlook.com>
2015-09-20 21:54:39 -0700

    works on ubuntu,move to windows

---
+++ dic.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+from bs4 import BeautifulSoup
+import tornado.httpclient
+
+cli = tornado.httpclient.HTTPClient()
+link='http://www.iciba.com/'
+serch=input('search: ')
+link+=serach
+data=cli.fetch(link)
+body=data.body.decode('utf8')
+soup=BeautifulSoup(body)
+
+group=soup.find_all(class_='group_pos')
+group2=group[0].find_all('p')
+for ele in group2:
+    print(ele.find(class_='fl').get_text())
+    result=ele.find_all('label')
+    for r in result:
+        print(r.get_text())
+        
